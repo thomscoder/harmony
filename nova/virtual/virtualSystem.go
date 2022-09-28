2b020a98587c2c1c1981ceafbafc02178ce96f32
thomscoder <moyelysian@icloud.com>
2022-09-28 15:38:25 +0200

    fix: dependency

--- nova/virtual/virtualSystem.go
+++ nova/virtual/virtualSystem.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/go-git/go-billy/v5"
 	"github.com/go-git/go-billy/v5/util"
+	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/storage/memory"
